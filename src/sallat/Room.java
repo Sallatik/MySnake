d125b965662f4cc1a70057db03ed49bd721be93c
Salim <niktonick@gmail.com>
2018-06-23 14:46:24 +0200

    Deleted Updater class, now using javax.swing.Timer

--- src/sallat/Room.java
+++ src/sallat/Room.java
@@ -33,7 +33,7 @@ enum Direction {
 class Room {
 	
 	public static final String VERSION = "2.1";
-	public static final int FPS = 25;
+	public static final int FPS = 50;
 	public static final int TICK = 100;
 	
 	private Matrix matrix;	 
@@ -90,9 +90,8 @@ class Room {
 		entities = new HashSet<Entity>();
 		
 		GameWindow window = new GameWindow(600,600,"sallat.Snake alpha ".concat(VERSION));
-		window.setUpdater(new Updater(FPS));
 		window.setJPanel(matrix);
-		
+		window.setFPS(FPS);
 	}
 	
 	public static void main(String [] args) {	
--- src/sallat/gui/GameWindow.java
+++ src/sallat/gui/GameWindow.java
@@ -4,7 +4,8 @@ import java.awt.BorderLayout;
 import javax.swing.*;
 public class GameWindow {
 	private JFrame frame;
-	private Updater updater;
+	private Timer timer;
+
 	public GameWindow(int width, int height, String title) {
 		frame = new JFrame(title);
 		frame.setSize(width,height);
@@ -12,17 +13,13 @@ public class GameWindow {
 		frame.setVisible(true);
 	}
 
-	public void setUpdater(Updater updater){
-		updater.setFrame(frame);
-		if (this.updater != null) 
-			this.updater.stop();
-		
-		this.updater = updater;
-		Thread updaterThread = new Thread(updater);
-		updaterThread.start();
-		
+	public void setFPS(int fps){
+		if(timer != null)
+			timer.stop();
+		timer = new Timer(1000/fps, e -> frame.repaint());
+		timer.start();
 	}
-	
+
 	public void setJPanel(JPanel panel) {
 		java.awt.Container contentPane = frame.getContentPane();
 		contentPane.removeAll();
